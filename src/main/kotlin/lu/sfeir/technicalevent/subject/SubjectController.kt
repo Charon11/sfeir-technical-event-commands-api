11bacf411a86cb4b666b8a174a7b06f56bb82d78
Renaud Chardin <chardin.r@sfeir.com>
2019-06-07 22:10:41 +0200

    route

--- src/main/kotlin/lu/sfeir/technicalevent/subject/SubjectController.kt
+++ src/main/kotlin/lu/sfeir/technicalevent/subject/SubjectController.kt
@@ -17,7 +17,7 @@ import lu.sfeir.technicalevent.subject.commands.refused.Refuse
 import lu.sfeir.technicalevent.subject.commands.refused.RefusedEvent
 import org.springframework.web.bind.annotation.*
 
-@RestController("/subject")
+@RestController
 class SubjectController(private val create: Create,
                         private val changeDescription: ChangeDescription,
                         private val accept: Accept,
@@ -25,30 +25,30 @@ class SubjectController(private val create: Create,
                         private val refuse: Refuse,
                         private val changeTitle: ChangeTitle) {
 
-    @PostMapping
+    @PostMapping("/subjects")
     fun add(@RequestBody createCommand: CreateCommand): CreatedEvent {
         return create.create(createCommand)
     }
 
-    @PutMapping("/{id}/accept")
+    @PutMapping("/subjects/{id}/accept")
     fun accept(@PathVariable id: String): AcceptedEvent {
         return accept.accept(id)
     }
 
-    @PutMapping("/{id}/refuse")
+    @PutMapping("/subjects/{id}/refuse")
     fun refuse(@PathVariable id: String): RefusedEvent {
         return refuse.refuse(id)
     }
-    @DeleteMapping("/{id}/delete")
+    @DeleteMapping("/subjects/{id}/delete")
     fun delete(@PathVariable id: String): DeletedEvent {
         return delete.delete(id)
     }
-    @PutMapping("/{id}/change-title")
+    @PutMapping("/subjects/{id}/change-title")
     fun changeTitle(@PathVariable id: String, @RequestBody changeTitleCommand: ChangeTitleCommand): TitleChangedEvent {
         return changeTitle.changeTitle(id, changeTitleCommand)
     }
 
-    @PutMapping("/{id}/change-description")
+    @PutMapping("/subjects/{id}/change-description")
     fun changeDescription(@PathVariable id: String, @RequestBody changeDescriptionCommand: ChangeDescriptionCommand): DescriptionChangedEvent {
         return changeDescription.changeDescription(id, changeDescriptionCommand)
     }
