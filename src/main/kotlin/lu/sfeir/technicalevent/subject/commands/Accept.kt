aa405b02ee8b7ffb1ecfe701ae1cfaf4be281bae
Renaud Chardin <chardin.r@sfeir.com>
2019-06-10 23:13:29 +0200

    Add scheduleDate

--- src/main/kotlin/lu/sfeir/technicalevent/subject/commands/Accept.kt
+++ src/main/kotlin/lu/sfeir/technicalevent/subject/commands/Accept.kt
@@ -6,15 +6,19 @@ import lu.sfeir.technicalevent.subject.EventKind
 import lu.sfeir.technicalevent.subject.Events
 import lu.sfeir.technicalevent.subject.SubjectStatus
 import org.springframework.stereotype.Service
+import java.time.Instant
 
 data class AcceptedEvent(val status: String,
+                         val scheduleDate: Instant,
                          override val entityId: String,
                          override val _kind: String = EventKind.ACCEPTED) : Events()
 
+data class AcceptCommand(val scheduleDate: Instant)
+
 @Service
 class Accept(private val gCloudPubSubService: GCloudPubSubService) {
-    fun accept(entityId: String): AcceptedEvent {
-        val accept = AcceptedEvent(SubjectStatus.ACCEPTED, entityId = entityId)
+    fun accept(entityId: String, acceptCommand: AcceptCommand): AcceptedEvent {
+        val accept = AcceptedEvent(SubjectStatus.ACCEPTED, scheduleDate = acceptCommand.scheduleDate, entityId = entityId)
         gCloudPubSubService.sendMessage(accept, ImmutableMap.of("_kind", accept._kind))
         return accept
     }
